3079fa68e9e97d370a1694a5a5065de862387091
georgewooding <georgewooding@hotmail.co.uk>
2017-08-04 23:19:50 +0100

    Modified to show bike example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,39 @@
 package test
 
-import mvnexample.Anotherexample
-import mvnexample.Example
-import mvnexample.anotherexample
-import mvnexample.example
+import mvnexample.BikeOuterClass.Bike
+import mvnexample.FrameOuterClass.Frame
+import mvnexample.MetalOuterClass.Metal
+import mvnexample.WheelOuterClass.Wheel
+import mvnexample.bike
+import mvnexample.frame
+import mvnexample.metal
+import mvnexample.wheel
 
 object Main extends App {
 
-  val nestedJava = Example.Hello.newBuilder.setHello(37)
-  val parentJava = Anotherexample.GoodBye.newBuilder().setHello(nestedJava).build
+  val metalBuilderJava = Metal.newBuilder.setMetalCode(1)
+  val frameBuilderJava = Frame.newBuilder.setFrameType(1)
+  val wheelBuilderJava = Wheel.newBuilder.setSpokes(1).setMetal(metalBuilderJava)
+  val bikeJava = Bike.newBuilder.setFrame(frameBuilderJava).setWheel(wheelBuilderJava).build
 
-  val nestedScala = example.Hello(hello = 37)
-  val parentScala = anotherexample.GoodBye(Some(nestedScala))
+  //Change metalType on wheel in a ridiculous fashion
 
-  val parentModifiedJava = parentJava.toBuilder.setHello(nestedJava.setHello(38)).build
-  val parentModifiedScala = parentScala.getHello.update(_.hello := 38)
+  val modifiedBikeJava = bikeJava.toBuilder.setWheel(wheelBuilderJava.setMetal(metalBuilderJava.setMetalCode(2))).build
+
+  //Or alternatively:
+
+  val modifiedMetalBuilderJava = metalBuilderJava.setMetalCode(2)
+  val modifiedWheelBuilderJava = wheelBuilderJava.setMetal(modifiedMetalBuilderJava)
+  val modifiedBikeAlternativeJava = modifiedBikeJava.toBuilder.setWheel(modifiedWheelBuilderJava).build
+
+  //now with Scala case classes:
+
+  val metalScala = metal.Metal(metalCode = 1)
+  val frameScala = frame.Frame(frameType = 1)
+  val wheelScala = wheel.Wheel(metal = Some(metalScala), spokes = 5)
+  val bikeScala = bike.Bike(frame = Some(frameScala), wheel = Some(wheelScala))
+
+  //Change metalType on wheel the ScalaPB way
+
+  val modifiedBikeScala = bikeScala.update(_.wheel.metal.metalCode := 2)
 }
