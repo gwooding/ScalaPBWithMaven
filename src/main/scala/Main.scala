ece5615317c90e0fe3d19a2fd2a2c9cf63fdca38
georgewooding <georgewooding@hotmail.co.uk>
2017-08-04 21:53:10 +0100

    Adds Java conversions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,19 @@
 package test
 
+import mvnexample.Anotherexample
+import mvnexample.Example.Hello
+
 object Main extends App {
-  println(mvnexample.example.Hello(hello = 37))
+
+  val nestedJava = Hello.newBuilder.setHello(37)
+
+  val parentJava = Anotherexample.GoodBye.newBuilder().setHello(nestedJava).build
+
+  val parentJavaModified = parentJava.toBuilder.setHello(nestedJava.setHello(38)).build
+
+  val nested = mvnexample.example.Hello(hello = 37)
+
+  val parent = mvnexample.anotherexample.GoodBye(Some(nested))
+
+  val parentModified = parent.getHello.update(_.hello := 38)
 }
